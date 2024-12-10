8b29c10f0d1674332749edc703933509c1407b0a
Jakob Rehmann <jakob.rehmann@gmail.com>
2024-12-10 16:52:41 +0100

    deleted core functionality for purpose of split repos

--- src/main/java/org/matsim/episim/model/ImmunityEvent.java
+++
@@ -1,7 +0,0 @@
-package org.matsim.episim.model;
-
-/**
- * An event which leads to an immune reaction, typical an infection or vaccination.
- */
-public interface ImmunityEvent {
-}
--- src/main/java/org/matsim/episim/model/vaccination/NoVaccination.java
+++
@@ -1,7 +0,0 @@
-package org.matsim.episim.model.vaccination;
-
-/**
- * Model will not vaccinate anybody.
- */
-public class NoVaccination implements VaccinationModel {
-}
